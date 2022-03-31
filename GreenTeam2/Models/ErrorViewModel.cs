f68e073694f70f15741f7de29b7cf8053ae8b70f
Polyc <Polyc@JOHN-DESKTOP>
2022-03-31 17:08:20 +0300

    green remove

--- GreenTeam2/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace GreenTeam2.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
